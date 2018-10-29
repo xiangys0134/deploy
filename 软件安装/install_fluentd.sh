ddd9e6f69379f3e5fb86c6813d66048c476cbaee
Your Name <you@example.com>
2018-10-29 13:40:30 +0800

    install_fluentd

--- 软件安装/install_fluentd.sh
+++ 软件安装/install_fluentd.sh
@@ -3,7 +3,7 @@
 # v1.0.1
 #CentOS7安装fluentd插件
 #
-[ . /etc/profile ] && . /etc/profile
+[ -f /etc/profile ] && . /etc/profile
 cmd=`pwd`
 
 function check_user() {
@@ -25,14 +25,14 @@ function install_rpm() {
     yum -y install zlib-devel libcurl-devel
     wget http://packages.treasuredata.com.s3.amazonaws.com/3/redhat/7/x86_64/td-agent-3.2.0-0.el7.x86_64.rpm
     if [ $? -ne 0 ]; then
-        echo "\033[31mtd-agent-3.2.0-0.el7.x86_64.rpm download failure\033[0m"
+        echo -e "\033[31mtd-agent-3.2.0-0.el7.x86_64.rpm download failure\033[0m"
         exit 6
     else
-        echo "\033[32mtd-agent-3.2.0-0.el7.x86_64.rpm download success\033[0m"
+        echo -e "\033[32mtd-agent-3.2.0-0.el7.x86_64.rpm download success\033[0m"
     fi
 
     if [ `check_rpm redhat-lsb-core` -eq 0 ]; then
-        echo "\033[32mredhat-lsb-core ready to install...\033[0m"
+        echo -e "\033[32mredhat-lsb-core ready to install...\033[0m"
         yum -y install redhat-lsb-core
     fi
 
@@ -42,8 +42,10 @@ function install_rpm() {
    
     sed -i -e '/^User=/c\User=root' -e '/^Group=/c\Group=root' /lib/systemd/system/td-agent.service
     systemctl daemon-reload
+
+    [ -f ${cmd}/td-agent-3.2.0-0.el7.x86_64.rpm ] && rm -rf ${cmd}/td-agent-3.2.0-0.el7.x86_64.rpm
     
-    echo "\033[32mstart up td-agent.service...\033[0m"
+    echo -e "\033[32mstart up td-agent.service...\033[0m"
     systemctl restart td-agent.service 
 }
 
