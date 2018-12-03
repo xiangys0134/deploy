6c5932339a21dddd64b7926daf7929ada8538eec
xiangys0134 <you@example.com>
2018-12-03 08:56:20 +0800

    rsync 1.0.4

--- Rsync/rsync.sh
+++ Rsync/rsync.sh
@@ -8,7 +8,7 @@
 
 cmd=`dirname $0`
 
-function beta_rsync() {
+function rsync_log() {
     #示例：rsync -vzrt -P --delete rsync_backup@172.4.0.12::tb03_184_log /BETA-log/tb03_184_log --password-file=/etc/rsyncd.pas
     #$1------> 172.4.0.12
     #$2------> rsync_backup
@@ -20,26 +20,26 @@ function beta_rsync() {
     rsync_pass_file=$3
     rsync_mod=$4
     dsc_directory=$5
-    rsync -vzrt -P --delete ${rsync_user}@${rsync_ip}::${rsync_mod} ${dsc_directory} --password-file=${rsync_pass_file}
+    #rsync -vzrt -P --delete ${rsync_user}@${rsync_ip}::${rsync_mod} ${dsc_directory} --password-file=${rsync_pass_file}
+    rsync -vzrt -P ${rsync_user}@${rsync_ip}::${rsync_mod} ${dsc_directory} --password-file=${rsync_pass_file}
 }
 
 pass_file=/etc/rsyncd.pas
 user=rsync_backup
-base_dir=/BETA-log
+base_dir=/release-log
 
-################################################################集中式web机#############################################################
-server_ip=172.4.0.5
+################################################################release web机01#############################################################
+server_ip=172.16.143.140
 server_mode="
-web_66_bms
-web_66_ipb
-web_66_irs
-web_66_oms
-web_66_rms
-web_66_utility
-web_66_pms
-web_66_uds
-web_66_pm2
-web_66_est
+web_140_bms
+web_140_ipb
+web_140_irs
+web_140_oms
+web_140_rms
+web_140_utility
+web_140_pms
+web_140_uds
+web_140_pm2
 "
 
 for mode in ${server_mode}
@@ -48,25 +48,23 @@ do
         mkdir ${base_dir}/${mode} -p
     fi
 
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
+    rsync_log "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
 
 done
 
 
-################################################################集中式脚本机#############################################################
-server_ip=172.4.0.6
+################################################################release web机02#############################################################
+server_ip=172.16.143.139
 server_mode="
-script_15_bms
-script_15_ipb
-script_15_irs
-script_15_oms
-script_15_rms
-script_15_utility
-script_15_pms
-script_15_uds
-script_15_SYSTEMS
-script_15_OS
-script_15_est
+web_139_bms
+web_139_ipb
+web_139_irs
+web_139_oms
+web_139_rms
+web_139_utility
+web_139_pms
+web_139_uds
+web_139_pm2
 "
 
 for mode in ${server_mode}
@@ -75,17 +73,22 @@ do
         mkdir ${base_dir}/${mode} -p
     fi
 
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
+    rsync_log "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
 
 done
 
 
-################################################################集中式TB1###############################################################
-server_ip=172.4.0.7
+################################################################release 脚本机#############################################################
+server_ip=172.16.143.138
 server_mode="
-tb01_211_log
-pbht_211_log
-file_entrust_211_log
+script_138_bms
+script_138_ipb
+script_138_irs
+script_138_oms
+script_138_rms
+script_138_utility
+script_138_pms
+script_138_uds
 "
 
 for mode in ${server_mode}
@@ -94,56 +97,15 @@ do
         mkdir ${base_dir}/${mode} -p
     fi
 
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
-
-done
-
-################################################################集中式TB2###############################################################
-server_ip=172.4.0.8
-server_mode="
-tb02_215_log
-"
-
-for mode in ${server_mode}
-do
-    if [ ! -d ${base_dir}/${mode} ]; then
-        mkdir ${base_dir}/${mode} -p
-    fi
-
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
-
-done
-
-################################################################集中式TB3###############################################################
-server_ip=172.4.0.12
-server_mode="
-tb03_184_log
-"
-
-for mode in ${server_mode}
-do
-    if [ ! -d ${base_dir}/${mode} ]; then
-        mkdir ${base_dir}/${mode} -p
-    fi
-
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
+    rsync_log "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
 
 done
 
 
-################################################################分布式机构端(券商版机构端web机)################################################
-server_ip=172.4.0.18
+################################################################release tb01#############################################################
+server_ip=10.80.60.135
 server_mode="
-web_238_bms
-web_238_ipb
-web_238_irs
-web_238_oms
-web_238_rms
-web_238_utility
-web_238_pms
-web_238_uds
-web_238_pm2
-web_238_est
+tb_135_log
 "
 
 for mode in ${server_mode}
@@ -152,23 +114,15 @@ do
         mkdir ${base_dir}/${mode} -p
     fi
 
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
+    rsync_log "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
 
 done
 
 
-################################################################分布式机构端(券商版机构端脚本机)################################################
-server_ip=172.4.0.17
+################################################################release tb02#############################################################
+server_ip=10.27.99.143
 server_mode="
-script_249_bms
-script_249_ipb
-script_249_irs
-script_249_oms
-script_249_rms
-script_249_utility
-script_249_pms
-script_249_uds
-script_249_est
+tb_143_log
 "
 
 for mode in ${server_mode}
@@ -177,22 +131,15 @@ do
         mkdir ${base_dir}/${mode} -p
     fi
 
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
+    rsync_log "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
 
 done
 
-################################################################分布式券商端(券商版券商端web机)################################################
-server_ip=172.4.0.15
+
+################################################################release tb03#############################################################
+server_ip=172.16.106.43
 server_mode="
-web_210_bms
-web_210_ipb
-web_210_irs
-web_210_oms
-web_210_rms
-web_210_utility
-web_210_pms
-web_210_uds
-web_210_pm2
+tb_43_log
 "
 
 for mode in ${server_mode}
@@ -201,21 +148,14 @@ do
         mkdir ${base_dir}/${mode} -p
     fi
 
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
+    rsync_log "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
 
 done
 
-################################################################分布式券商端(券商版券商端脚本机)################################################
-server_ip=172.4.0.14
+################################################################release tb04#############################################################
+server_ip=10.30.200.93
 server_mode="
-script_122_bms
-script_122_ipb
-script_122_irs
-script_122_oms
-script_122_rms
-script_122_utility
-script_122_pms
-script_122_uds
+tb_93_log
 "
 
 for mode in ${server_mode}
@@ -224,7 +164,7 @@ do
         mkdir ${base_dir}/${mode} -p
     fi
 
-    beta_rsync "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
+    rsync_log "${server_ip}" "${user}" "${pass_file}" "${mode}" "${base_dir}/${mode}"
 
 done
 
