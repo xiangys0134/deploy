5c695178ad16621ee54827e2121bc93edf02a2a6
ops <ops@xuncetech.com>
2019-05-16 11:07:44 +0800

    v1.0.10

--- jenkins/rpm包自动打包/xone/scada-task.sh
+++ jenkins/rpm包自动打包/xone/scada-task.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # yousong.xiang 250919938@qq.com
-# 2019.5.10
-# v1.0.1
+# 2019.5.16
+# v1.0.2
 # jenkins构建脚本
 
 [ -f /etc/profile ] && . /etc/profile
 ftp_dir=/data/vsftpd_data/admin
+ftp_host='192.168.40.148'
+#echo "${WORKSPACE}"
+#echo "${JENKINS_HOME}/${WORKSPACE}/${JOB_NAME}"
+
 
 function build_scadatask() {    
     ver='1.0.0'
@@ -19,15 +23,22 @@ function build_scadatask() {
         fi
     else
         tag="yes"
-        ver=`echo ${select} |awk -F "" '{for(i=2;i<=NF;i++){printf $i}}'`        
+        
     fi
     cd ${WORKSPACE}
+    echo ""
+    echo "## maven build start time: $(date +"%Y-%m-%d %H:%M:%S")"
+    echo ""
     mvn clean compile verify -P prod -Dmaven.test.skip=true
     if [ $? -ne 0 ]; then
         echo "mvn buld failed!"
         exit 4
     fi
-    cd ${WORKSPACE}/target/${JOB_NAME}-[0-9]*.[0-9]*.[0-9]*-SNAPSHOT-dist/
+    echo ""
+    echo "## maven build stop time: $(date +"%Y-%m-%d %H:%M:%S")"
+    echo ""
+    #cd ${WORKSPACE}/target/${JOB_NAME}-[0-9]*.[0-9]*.[0-9]*-SNAPSHOT-dist/
+    cd ${WORKSPACE}/target/${JOB_NAME}-*
     mv ${JOB_NAME} ${JOB_NAME}-${ver}
     tar -czf ${JOB_NAME}-${ver}.tar.gz ${JOB_NAME}-${ver} 
 }
@@ -35,23 +46,49 @@ function build_scadatask() {
 function backup_scadatask() {
     if [ "${tag}" == "dev" ]; then
         if [ ! -d ${ftp_dir}/XONE/${JOB_NAME}/dev ]; then
-            mkdir -p ${ftp_dir}/XONE/${JOB_NAME}/dev
+            sudo mkdir -p ${ftp_dir}/XONE/${JOB_NAME}/dev
         fi
-        cd ${WORKSPACE}/target/${JOB_NAME}-[0-9]*.[0-9]*.[0-9]*-SNAPSHOT-dist/
-        mv ${JOB_NAME}-${ver}.tar.gz ${ftp_dir}/XONE/${JOB_NAME}/dev
+        #cd ${WORKSPACE}/target/${JOB_NAME}-[0-9]*.[0-9]*.[0-9]*-SNAPSHOT-dist/
+        cd ${WORKSPACE}/target/${JOB_NAME}-*
+        sudo mv ${JOB_NAME}-${ver}.tar.gz ${ftp_dir}/XONE/${JOB_NAME}/dev
+        if [ $? -eq 0 ]; then
+            echo "upload file success"
+            echo "ftp://${ftp_host}/XONE/${JOB_NAME}/dev/${JOB_NAME}-${ver}.tar.gz"
+        else
+            echo "upload file failed"
+            exit 5
+        fi
     elif [ "${tag}" == "yes" ]; then
         if [ -d ${ftp_dir}/XONE/${JOB_NAME}/tag ]; then
-            mkdir -p ${ftp_dir}/XONE/${JOB_NAME}/tag
+            sudo mkdir -p ${ftp_dir}/XONE/${JOB_NAME}/tag
         fi
-        cd ${WORKSPACE}/target/${JOB_NAME}-[0-9]*.[0-9]*.[0-9]*-SNAPSHOT-dist/
-        mv ${JOB_NAME}-${ver}.tar.gz ${ftp_dir}/XONE/${JOB_NAME}/tag
+        #cd ${WORKSPACE}/target/${JOB_NAME}-[0-9]*.[0-9]*.[0-9]*-SNAPSHOT-dist/
+        cd ${WORKSPACE}/target/${JOB_NAME}-*
+        sudo mv ${JOB_NAME}-${ver}.tar.gz ${ftp_dir}/XONE/${JOB_NAME}/tag
+        if [ $? -eq 0 ]; then
+            echo "upload file success"
+            echo "ftp://${ftp_host}/XONE/${JOB_NAME}/tag/${JOB_NAME}-${ver}.tar.gz"
+        else
+            echo "upload file failed"
+            exit 6
+        fi
     else
         if [ ! -d ${ftp_dir}/XONE/${JOB_NAME}/brach ]; then
-            mkdir ${ftp_dir}/XONE/${JOB_NAME}/brach
+            sudo mkdir -p ${ftp_dir}/XONE/${JOB_NAME}/brach
         fi
-        cd ${WORKSPACE}/target/${JOB_NAME}-[0-9]*.[0-9]*.[0-9]*-SNAPSHOT-dist/
-        mv ${JOB_NAME}-${ver}.tar.gz ${ftp_dir}/XONE/${JOB_NAME}/brach
+        #cd ${WORKSPACE}/target/${JOB_NAME}-[0-9]*.[0-9]*.[0-9]*-SNAPSHOT-dist/
+        cd ${WORKSPACE}/target/${JOB_NAME}-*
+        sudo mv ${JOB_NAME}-${ver}.tar.gz ${ftp_dir}/XONE/${JOB_NAME}/brach
+        if [ $? -eq 0 ]; then
+            echo "upload file success"
+            echo "ftp://${ftp_host}/XONE/${JOB_NAME}/brach/${JOB_NAME}-${ver}.tar.gz"
+        else
+            echo "upload file failed"
+            exit 6
+        fi
+
     fi
+
 }
 
 build_scadatask
