bd2e0b95520221a11600cba6af13fed0d0b6a690
Your Name <you@example.com>
2021-08-26 09:12:26 +0800

    mysqlbak

--- 备份/mysql8bak.sh
+++ 备份/mysql8bak.sh
@@ -16,7 +16,7 @@ dbuser='mysqldumper'
 dbpasswd='123456'
 #备份时间
 backtime=`date '+%Y%m%d%H%M%S'`
-t_time=`date '+%Y-%m-%d %H:%M:%S'`
+#t_time=`date '+%Y-%m-%d %H:%M:%S'`
 
 #日志备份路径
 logpath='/data/mysqlbakup'
@@ -28,23 +28,27 @@ if [ ! -d ${datapath} ];then
 fi
 
 #日志记录头部
-echo "备份时间为${t_time},备份数据库表 ${dbname} 开始" >> ${logpath}/mysqllog.log
+#echo "备份时间为${t_time},备份数据库表 ${dbname} 开始" >> ${logpath}/mysqllog.log
 
 #获取数据库名
 dbname=`mysql -h ${dbhost} -u${dbuser} -p${dbpasswd} -e "show databases;" |egrep -v "Database|sys|information_schema|mysql|performance_schema|awsdms_control|test"`
 
 #正式备份数据库
 for db in $dbname; do
+  t_time=`date '+%Y-%m-%d %H:%M:%S'`
+  echo "${t_time} 数据库 ${db} 开始备份..." >> ${logpath}/mysqllog.log
   mysqldump -h ${dbhost} -u${dbuser} -p${dbpasswd} -F -B $db --source-data=2 --single-transaction |gzip> ${logpath}/${db}${backtime}.sql.gz 2>> ${logpath}/mysqllog.log
   #备份成功以下操作
   if [ "$?" == 0 ];then
+    t_time=`date '+%Y-%m-%d %H:%M:%S'`
     echo "${t_time} 数据库 ${db} 备份成功!!" >> ${logpath}/mysqllog.log
   else
     #备份失败则进行以下操作
+    t_time=`date '+%Y-%m-%d %H:%M:%S'`
     echo "${t_time} 数据库 ${db} 备份失败!!" >> ${logpath}/mysqllog.log
   fi
 done
 
 #删除七天前备份，也就是只保存7天内的备份
 cd ${datapath:-/tmp}
-find ${datapath:-/tmp/mysqlbakup} -name "*.gz" -type f -mtime +7 -exec rm -rf {} \; > /dev/null 2>&1
+find ${datapath:-/tmp/mysqlbakup} -name "*.gz" -type f -mtime +3 -exec rm -rf {} \; > /dev/null 2>&1
